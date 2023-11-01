28168d3c52da39bea37b0dc84367d79fe0aa408e
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-11-01 05:45:54 -0500

    Change http to https

--- src/fetch.js
+++ src/fetch.js
@@ -1,6 +1,6 @@
 const getWeather = async (loc = "Amsterdam") => {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/forecast.json?key=76476d4683bf484d83094151232810&q=${loc}&days=3&aqi=yes`,
+    `https://api.weatherapi.com/v1/forecast.json?key=76476d4683bf484d83094151232810&q=${loc}&days=3&aqi=yes`,
   );
   const locationData = await response.json();
   return locationData;
